1fe87a3dae2c0a3a2374e7e5889b896e343ab8cf
josefa_santana <santanadejesusj@gmail.com>
2023-07-12 22:44:02 -0300

    Book repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/BookMinDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Book;
+
+public class BookMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public BookMinDTO() {
+	}
+
+	public BookMinDTO(Book entity) {
+		id               = entity.getId();
+		title            = entity.getTitle();
+		year             = entity.getYear();
+		imgUrl           = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+	
+}
